71767b9a2a7955d2f1a61144efcf7080ab81f4d9
Klemen Kenda <klemen.kenda@ijs.si>
2022-05-13 10:04:54 +0300

    Tab cleanup

--- apps/index.NAIADES.alicante_features_raw.py
+++ apps/index.NAIADES.alicante_features_raw.py
@@ -17,20 +17,20 @@ measurements_conductivity = [
     'salinity_EA003_36_conductivity',
     'salinity_EA007_36_conductivity',
     'salinity_EA008_36_conductivity'
-    ]
+]
 
 
 #start = 14 #1
 stop = 9   #0
 
 template = {
-            "aggregate":"mean",
-            "measurement":"alicante",
-            "fields":["value"],
-            "tags":{None: None},
-            "window":"1m",
-            "when":"-0h"
-            }
+    "aggregate":"mean",
+    "measurement":"alicante",
+    "fields":["value"],
+    "tags":{None: None},
+    "window":"1m",
+    "when":"-0h"
+}
 
 fusions = {}
 
@@ -44,18 +44,18 @@ for m in measurements_conductivity:
 
 def RunBatchFusionOnce():
     for location in measurements_conductivity:
-      today = datetime.datetime.today()
+        today = datetime.datetime.today()
 
-      config = {
-          "token":"k_TK7JanSGbx9k7QClaPjarlhJSsh8oApCyQrs9GqfsyO3-GIDf_tJ79ckwrcA-K536Gvz8bxQhMXKuKYjDsgw==",
-          "url": "http://localhost:8086",
-          "organisation": "naiades",
-          "bucket": "alicante",
-          "startTime":"2021-07-07T00:00:00",
-          "stopTime":"2021-07-13T00:00:00",
-          "every":"1m",
-          "fusion": fusions[location]
-      }
+        config = {
+            "token":"k_TK7JanSGbx9k7QClaPjarlhJSsh8oApCyQrs9GqfsyO3-GIDf_tJ79ckwrcA-K536Gvz8bxQhMXKuKYjDsgw==",
+            "url": "http://localhost:8086",
+            "organisation": "naiades",
+            "bucket": "alicante",
+            "startTime":"2021-07-07T00:00:00",
+            "stopTime":"2021-07-13T00:00:00",
+            "every":"1m",
+            "fusion": fusions[location]
+        }
 
       today = datetime.datetime.today()
       folder = 'features_data'
@@ -65,12 +65,12 @@ def RunBatchFusionOnce():
 
       #print(config['stopTime'] )
       with open(f'{folder}/features_alicante_{location}_raw.json', 'w+')as file_json:
-        try:
-          lines = file_json.readlines()
-          last_line = lines[-1]
-          tss = int(json.loads(last_line)['timestamp']/1000 + 60*60)
-        except:
-          tss = 1649000000
+          try:
+              lines = file_json.readlines()
+              last_line = lines[-1]
+              tss = int(json.loads(last_line)['timestamp']/1000 + 60*60)
+          except:
+              tss = 1649000000
 
       config['startTime'] = datetime.datetime.utcfromtimestamp(tss).strftime("%Y-%m-%dT%H:%M:00")
 
@@ -82,29 +82,28 @@ def RunBatchFusionOnce():
 
       sf2 = batchFusion(config)
 
-
       update_outputs = True
       try:
-        fv, t = sf2.buildFeatureVectors()
+          fv, t = sf2.buildFeatureVectors()
       except:
-        print('Feature vector generation failed')
-        update_outputs = False
+          print('Feature vector generation failed')
+          update_outputs = False
 
       if(update_outputs):
-        with open(f'{folder}/features_alicante_{location}_raw.json', 'a+') as file_json:
-          for j in range(t.shape[0]):
-              fv_line = {"timestamp":int(t[j].astype('uint64')/1000000), "ftr_vector":list(fv[j])}
-              if((all(isinstance(x, (float, int)) for x in fv[j])) and (not np.isnan(fv[j]).any())):
-                file_json.write((json.dumps(fv_line) + '\n' ))
-          file_json.close()
+          with open(f'{folder}/features_alicante_{location}_raw.json', 'a+') as file_json:
+              for j in range(t.shape[0]):
+                  fv_line = {"timestamp":int(t[j].astype('uint64')/1000000), "ftr_vector":list(fv[j])}
+                  if((all(isinstance(x, (float, int)) for x in fv[j])) and (not np.isnan(fv[j]).any())):
+                      file_json.write((json.dumps(fv_line) + '\n' ))
+              file_json.close()
 
 
         for j in range(t.shape[0]):
             output = {"timestamp":int(t[j].astype('uint64')/1000000), "ftr_vector":list(fv[j])}
             output_topic = f"features_alicante_{location}"
-           # Start Kafka producer
+            # Start Kafka producer
             if((all(isinstance(x, (float, int)) for x in fv[j])) and (not np.isnan(fv[j]).any())):
-              future = producer.send(output_topic, output)
+                future = producer.send(output_topic, output)
 
             try:
                 record_metadata = future.get(timeout=10)
@@ -121,6 +120,5 @@ print("Current Time =", current_time)
 
 RunBatchFusionOnce()
 while True:
-
     schedule.run_pending()
     time.sleep(1)
