1fbf0b1db0e6af55cf51eafc749040952fd83818
Klemen Kenda <klemen.kenda@ijs.si>
2022-05-13 10:40:50 +0300

    Adding comments and cleanup

--- apps/index.NAIADES.alicante_features_raw.py
+++ apps/index.NAIADES.alicante_features_raw.py
@@ -1,3 +1,9 @@
+"""Module for data fusion for Alicante (NAIADES).
+
+This module creates feature vectors from InfluxDB data for ... __TODO__.
+"""
+
+# imports
 from src.fusion.stream_fusion import streamFusion, batchFusion
 
 import pandas as pd
@@ -10,8 +16,15 @@ import schedule
 
 from kafka import KafkaProducer
 
+
+# constants -- to be moved to configuration structure (TODO)
+config_folder = "config" # TODO: should this be renamed to "current_config" or similar?
+features_folder = 'features_data'
+
+# initializing kafka producer
 producer = KafkaProducer(bootstrap_servers="localhost:9092", value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
+# building fusion configs
 measurements_conductivity = [
     'salinity_EA002_26_conductivity',
     'salinity_EA003_36_conductivity',
@@ -28,7 +41,7 @@ template = {
     "when":"-0h"
 }
 
-fusions = {}
+fusions = {} # TODO - should this be fusions = [], because it is an array, not a list?
 
 for m in measurements_conductivity:
     fusion = []
@@ -37,10 +50,18 @@ for m in measurements_conductivity:
     fusion.append(temp)
     fusions[m] = copy.deepcopy(fusion)
 
+
 def RunBatchFusionOnce():
+    """Runs batch fusion for Alicante once.
+
+    Docs needed ... __TODO___
+    """
+
+    # run data fusion for each of the locations
     for location in measurements_conductivity:
         today = datetime.datetime.today()
 
+        # config template -- TODO: this should be moved to an outside static config structure (as JSON)
         config = {
             "token":"k_TK7JanSGbx9k7QClaPjarlhJSsh8oApCyQrs9GqfsyO3-GIDf_tJ79ckwrcA-K536Gvz8bxQhMXKuKYjDsgw==",
             "url": "http://localhost:8086",
@@ -52,14 +73,13 @@ def RunBatchFusionOnce():
             "fusion": fusions[location]
         }
 
+        # feature vectors should be generated up to the last timestamp
         today = datetime.datetime.today()
-        folder = 'features_data'
-
         config['stopTime'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:00")
-        #config['startTime'] = datetime.datetime.utcfromtimestamp((today - datetime.datetime(1970, 1, 2 + start)).total_seconds()).strftime("%Y-%m-%dT%H:00:00")
 
-        #print(config['stopTime'] )
-        with open(f'{folder}/features_alicante_{location}_raw.json', 'w+')as file_json:
+        # try determining the last time, a feature vector was generated for
+        # and set it as a start time
+        with open(f'{features_folder}/features_alicante_{location}_raw.json', 'w+') as file_json:
             try:
                 lines = file_json.readlines()
                 last_line = lines[-1]
@@ -69,37 +89,59 @@ def RunBatchFusionOnce():
 
         config['startTime'] = datetime.datetime.utcfromtimestamp(tss).strftime("%Y-%m-%dT%H:%M:00")
 
-        #print(config['startTime'])
-
-        file_json = open(f'alicante_salinity_{location}_raw_config.json', 'w')
-        file_json.write(json.dumps(config, indent=4, sort_keys=True) )
+        # write current config for data fusion into JSON file
+        file_json = open(f'{config_folder}/alicante_salinity_{location}_raw_config.json', 'w')
+        file_json.write(json.dumps(config, indent=4, sort_keys=True))
         file_json.close()
 
+        # initialize batch fusion
         sf2 = batchFusion(config)
 
+        # by default we expect that new data will be generated and that the
+        # outputs (Kafka and files) should be updated
         update_outputs = True
         try:
+            # building feature vectors
             fv, t = sf2.buildFeatureVectors()
         except:
-            print('Feature vector generation failed')
+            print('Feature vector generation failed.')
+            # TODO: print error too
             update_outputs = False
 
-        if(update_outputs):
-            with open(f'{folder}/features_alicante_{location}_raw.json', 'a+') as file_json:
+        # writing to a file
+        if (update_outputs):
+            with open(f'{features_folder}/features_alicante_{location}_raw.json', 'a+') as file_json:
+                # go through all generated feature vectors
                 for j in range(t.shape[0]):
-                    fv_line = {"timestamp":int(t[j].astype('uint64')/1000000), "ftr_vector":list(fv[j])}
-                    if((all(isinstance(x, (float, int)) for x in fv[j])) and (not np.isnan(fv[j]).any())):
-                        file_json.write((json.dumps(fv_line) + '\n' ))
-                file_json.close()
+                    # generate correct format of a feature vector
+                    fv_line = {
+                        "timestamp": int(t[j].astype('uint64')/1000000),
+                        "ftr_vector": list(fv[j])
+                    }
 
+                    # write a feature vector into the log file only if all the features are
+                    # in numeric format
+                    if ((all(isinstance(x, (float, int)) for x in fv[j])) and (not np.isnan(fv[j]).any())):
+                        file_json.write((json.dumps(fv_line) + '\n'))
 
+                file_json.close() # TODO: is this really needed, does not a file close by itself after "with" is finished
+
+        # writing to kafka
+        # TODO: all the structures and checks in this and previous block are the same
+        #       so these blocks should be joined together
         for j in range(t.shape[0]):
-            output = {"timestamp":int(t[j].astype('uint64')/1000000), "ftr_vector":list(fv[j])}
+            # create output structure for kafka
+            output = {
+                "timestamp":int(t[j].astype('uint64')/1000000),
+                "ftr_vector":list(fv[j])
+            }
             output_topic = f"features_alicante_{location}"
-            # Start Kafka producer
+
+            # start kafka producer and send the data
             if((all(isinstance(x, (float, int)) for x in fv[j])) and (not np.isnan(fv[j]).any())):
                 future = producer.send(output_topic, output)
 
+            # check sending to kafka
             try:
                 record_metadata = future.get(timeout=10)
             except Exception as e:
