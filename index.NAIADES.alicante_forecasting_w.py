743a960aeeb15f9782f5570ad00c684615ad326e
Klemen Kenda <klemen.kenda@ijs.si>
2022-08-23 12:12:19 +0200

    Cleanup of the code + commenting - part 1

--- index.NAIADES.alicante_forecasting_w.py
+++ index.NAIADES.alicante_forecasting_w.py
@@ -1,5 +1,4 @@
-from src.fusion.stream_fusion import streamFusion, batchFusion
-
+# includes
 import pandas as pd
 import numpy as np
 import json
@@ -7,11 +6,25 @@ import copy
 import time
 import datetime
 import schedule
+import logging
 
 from kafka import KafkaProducer
 
+# project-based includes
+from src.fusion.stream_fusion import streamFusion, batchFusion
+
+# logger initialization
+LOGGER = logging.getLogger(__name__)
+logging.basicConfig(
+    format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s", level=logging.INFO)
+
+# starting Kafka producer
 producer = KafkaProducer(bootstrap_servers="localhost:9092", value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
+# CONFIG generations ---------------------------------------------
+LOGGER.info("Generating Alicante configurations")
+
+# Alicante locations definition for config generation
 locations = [
     'alipark',
     'autobuses',
@@ -20,35 +33,37 @@ locations = [
     'mercado',
     'montaneta',
     'rambla'
-    ]
+]
 
-#start = 14 #1
-stop = 9   #0
-
+# set of fusions
 fusions = {}
 
-
+# iterating through all the Alicante locations
 for m in locations:
     fusion = []
+    # general template for a feature
     template = {
-            "aggregate":"mean",
-            "measurement":"alicante",
-            "fields":["value"],
-            "tags":{None: None},
-            "window":"30m",
-            "when":"-0h"
-            }
+        "aggregate": "mean",
+        "measurement": "alicante",
+        "fields": ["value"],
+        "tags": {None: None},
+        "window": "30m",
+        "when": "-0h"
+    }
+
+    # adding flow features (12-hour profile with 30min resolution)
     template['measurement'] = f'{m}_flow'
     for i in range(24):
         temp = copy.deepcopy(template)
         temp['fields'] = ["value"]
+        # 12-hour profile of with 30min resolution
         temp['when'] = f'-{(23-i)*30}m'
         fusion.append(temp)
 
-
+    # fixing template for weather data (12-hour hourly profile)
     template['measurement'] = 'weather_observed1'
     template['window'] = "1h"
-            
+
     for i in range(12):
         temp = copy.deepcopy(template)
         temp['fields'] = ["pressure"]
@@ -74,118 +89,94 @@ for m in locations:
         temp['fields'] = ["wind_speed"]
         temp['when'] = f'-{(11-i)}h'
         fusion.append(temp)
+
+    # adding fusion to the set of all fusions
     fusions[m] = copy.deepcopy(fusion)
 
 
-
-
-
-#while (True):
-wait = True
-once = True
+# FUNCTION definition --------------------------------------------
 
 def RunBatchFusionOnce():
+    # iterate through all the locations
     for location in locations:
-      today = datetime.datetime.today()
+        # template for the batch fusion config
+        config = {
+            "token": "k_TK7JanSGbx9k7QClaPjarlhJSsh8oApCyQrs9GqfsyO3-GIDf_tJ79ckwrcA-K536Gvz8bxQhMXKuKYjDsgw==",
+            "url": "http://localhost:8086",
+            "organisation": "naiades",
+            "bucket": "alicante",
+            "startTime": "2022-08-15T00:00:00",
+            "stopTime": "2022-08-16T00:00:00",
+            "every": "1h",
+            "fusion": fusions[location]
+        }
 
-      config = {
-          "token":"k_TK7JanSGbx9k7QClaPjarlhJSsh8oApCyQrs9GqfsyO3-GIDf_tJ79ckwrcA-K536Gvz8bxQhMXKuKYjDsgw==",
-          "url": "http://localhost:8086",
-          "organisation": "naiades",
-          "bucket": "alicante",
-          "startTime":"2021-07-07T00:00:00",
-          "stopTime":"2021-07-13T00:00:00",
-          "every":"1h",
-          "fusion": fusions[location]
-      }
+        today = datetime.datetime.today()
+        features_folder = 'features_data'
+        config['stopTime'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:00:00")
 
-      #print(json.dumps(config, indent=4, sort_keys=True))
+        # reading last generating feature for obtaining last successful timestamp
+        file_json = open(f'{features_folder}/features_alicante_{location}_forecasting_w.json', 'r')
+        lines = file_json.readlines()
+        last_line = lines[-1]
+        tss = int(json.loads(last_line)['timestamp']/1000 + 30 * 60)
 
-      today = datetime.datetime.today()
-      folder = 'features_data'
+        config['startTime'] = datetime.datetime.utcfromtimestamp(tss).strftime("%Y-%m-%dT%H:00:00")
+        file_json = open(f'alicante_{location}_forecasting_w_config.json', 'w')
+        file_json.write(json.dumps(config, indent=4, sort_keys=True) )
+        file_json.close()
 
-      config['stopTime'] = datetime.datetime.now().strftime("%Y-%m-%dT%H:00:00")
-      #config['startTime'] = datetime.datetime.utcfromtimestamp((today - datetime.datetime(1970, 1, 2 + start)).total_seconds()).strftime("%Y-%m-%dT%H:00:00")
-
-      #print(config['stopTime'] )
-
-      file_json = open(f'{folder}/features_alicante_{location}_forecasting_w.json', 'r')
-
-      lines = file_json.readlines()
-      last_line = lines[-1]
-      tss = int(json.loads(last_line)['timestamp']/1000 + 30*60)
-
-      #print(last_line)
-      #print(tss)
-      #print(datetime.datetime.utcfromtimestamp(tss).strftime("%Y-%m-%dT%H:00:00"))
-
-      config['startTime'] = datetime.datetime.utcfromtimestamp(tss).strftime("%Y-%m-%dT%H:00:00")
-
-      #print(config['startTime'])
-
-      file_json = open(f'alicante_{location}_forecasting_w_config.json', 'w')
-      file_json.write(json.dumps(config, indent=4, sort_keys=True) )
-      file_json.close()
-
-      #sf2 = batchFusion(config)
-      sf2 = batchFusion(config)
+        sf2 = batchFusion(config)
 
 
-      update_outputs = True
-      try:
-        fv, t = sf2.buildFeatureVectors()
-      except:
-        print('Feature vector generation failed')
-        update_outputs = False
+        update_outputs = True
+        try:
+            fv, t = sf2.buildFeatureVectors()
+        except:
+            print('Feature vector generation failed')
+            update_outputs = False
 
-      if(update_outputs):
+        if(update_outputs):
 
-        consumption_tosend = []
-        for i in range(len(t)):
-            
-            Flow = fv[i, :24]
-            Weather = fv[i, 24:]
-            weather_ext = np.zeros(len(Weather)*2)
-            weather_ext[::2] = Weather
-            weather_ext[1::2] = Weather
+            consumption_tosend = []
+            for i in range(len(t)):
 
-            vec = np.concatenate([Flow, weather_ext])
-            consumption_tosend.append(vec)
-            last_values = vec[23::24]
-          
-            if(not pd.isna(last_values).any()):
-                fv_line = {"timestamp":int(t[i].astype('uint64')/1000000), "ftr_vector":list(consumption_tosend[i])}
-                
-                #data is uploaded at different times - this ensures that FV's won't be sent if data hasn't been uploaded for one or more of the sensors
-                with open(f'{folder}/features_alicante_{location}_forecasting_w.json', 'a') as file_json:
-                  file_json.write((json.dumps(fv_line) + '\n' ))
-                
-                file_json.close()
-                
-                output = {"timestamp":int(t[i].astype('uint64')/1000000), "ftr_vector":list(consumption_tosend[i])}
-                output_topic = f'features_alicante_{location}_forecasting_w'
-      
-                future = producer.send(output_topic, output)
-        
-                try:
-                    record_metadata = future.get(timeout=10)
-                except Exception as e:
-                    print('Producer error: ' + str(e))
-                      
+                Flow = fv[i, :24]
+                Weather = fv[i, 24:]
+                weather_ext = np.zeros(len(Weather)*2)
+                weather_ext[::2] = Weather
+                weather_ext[1::2] = Weather
 
-#Do batch fusion once per hour
+                vec = np.concatenate([Flow, weather_ext])
+                consumption_tosend.append(vec)
+                last_values = vec[23::24]
 
+                if(not pd.isna(last_values).any()):
+                    fv_line = {"timestamp":int(t[i].astype('uint64')/1000000), "ftr_vector":list(consumption_tosend[i])}
 
+                    #data is uploaded at different times - this ensures that FV's won't be sent if data hasn't been uploaded for one or more of the sensors
+                    with open(f'{folder}/features_alicante_{location}_forecasting_w.json', 'a') as file_json:
+                    file_json.write((json.dumps(fv_line) + '\n' ))
+
+                    file_json.close()
+
+                    output = {"timestamp":int(t[i].astype('uint64')/1000000), "ftr_vector":list(consumption_tosend[i])}
+                    output_topic = f'features_alicante_{location}_forecasting_w'
+
+                    future = producer.send(output_topic, output)
+
+                    try:
+                        record_metadata = future.get(timeout=10)
+                    except Exception as e:
+                        print('Producer error: ' + str(e))
+
+# MAIN part of the program -------------------------------
+# create hourly scheduler
 schedule.every().hour.do(RunBatchFusionOnce)
-
-now = datetime.datetime.now()
-
-current_time = now.strftime("%H:%M:%S")
-print("Current Time =", current_time)
-
 RunBatchFusionOnce()
-print('Component started successfully.')
-while True:
+LOGGER.info('Component started successfully.')
 
+# checking scheduler
+while True:
     schedule.run_pending()
     time.sleep(1)
