88d4cb8c28aa101c5650ca0a61f8f2c4757fc79d
Klemen Kenda <klemen.kenda@ijs.si>
2022-09-17 10:47:16 +0200

    Cleanup

--- index.NAIADES.leakage_pressure_updated.py
+++ index.NAIADES.leakage_pressure_updated.py
@@ -7,40 +7,57 @@ import copy
 import time
 import datetime
 import schedule
+import logging
 
 from kafka import KafkaProducer
 
+# logger initialization
+LOGGER = logging.getLogger(__name__)
+logging.basicConfig(
+    format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s", level=logging.INFO)
+
+# import secrets
+with open("secrets.json", "r") as jsonfile:
+    secrets = json.load(jsonfile)
+    print(secrets)
+
+# starting Kafka producer
 producer = KafkaProducer(bootstrap_servers="localhost:9092", value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
+# definition of sensors; flow and pressure
 measurements_analog = [
     #'flow211106H360',
     'flow211206H360',
     'flow211306H360',
     'flow318505H498'
-    ]
+]
+
 measurements_presure = [
     'pressure5770',
     'pressure5771',
     'pressure5772',
     'pressure5773'
-    ]
+]
 
-#start = 14 #1
-stop = 9   #0
-
+# template for flow
 template = {
-            "aggregate":"mean",
-            "measurement":"braila",
-            "fields":["analog_input2"],
-            "tags":{None: None},
-            "window":"2h",
-            "when":"-1h"
-            }
+    "aggregate": "mean",
+    "measurement": "braila",
+    "fields": ["analog_input2"],
+    "tags": {None: None},
+    "window": "2h",
+    "when": "-1h"
+}
+
+# list of data fusions
 fusion = []
+
+# iterate through flow sensors
 for m in measurements_analog:
     template['measurement'] = m
     for i in range(1):
         temp = copy.deepcopy(template)
+        # make an exception for new sensor
         if(m == 'flow318505H498'):
             temp['window'] = '4h'
         else:
@@ -48,37 +65,32 @@ for m in measurements_analog:
         temp['when'] = f'-{i}h'
         fusion.append(temp)
 
-
+# update template for pressure
 template['fields'] = ['value']
 template['window'] = '1h'
+
+# iterate through pressure sensors
 for m in measurements_presure:
     template['measurement'] = m
-
     for i in range(1):
         temp = copy.deepcopy(template)
         temp['when'] = f'-{i}h'
         fusion.append(temp)
 
-#while (True):
-wait = True
-once = True
-
+# batch fusion definition
 def RunBatchFusionOnce():
-    today = datetime.datetime.today()
-
     config = {
-        "token":"k_TK7JanSGbx9k7QClaPjarlhJSsh8oApCyQrs9GqfsyO3-GIDf_tJ79ckwrcA-K536Gvz8bxQhMXKuKYjDsgw==",
+        "token": secrets["influx_token"],
         "url": "http://localhost:8086",
         "organisation": "naiades",
         "bucket": "braila",
-        "startTime":"2021-07-07T00:00:00",
-        "stopTime":"2021-07-13T00:00:00",
-        "every":"1h",
+        "startTime": secrets["start_time"],
+        "stopTime": secrets["stop_time"],
+        "every": "1h",
         "fusion": fusion
     }
 
-    today = datetime.datetime.today()
-
+    # folders for storing features and config data
     features_folder = 'features_data'
     config_folder = 'config_data'
 
@@ -86,86 +98,83 @@ def RunBatchFusionOnce():
 
     with open(f'{features_folder}/leakage_pressure_updated.json', 'a+')as file_json:
         try:
-          lines = file_json.readlines()
-          last_line = lines[-1]
-          tss = int(json.loads(last_line)['timestamp']/1000 + 60*60)
+            # updating start time from the last successfully obtained feature
+            lines = file_json.readlines()
+            last_line = lines[-1]
+            tss = int(json.loads(last_line)['timestamp']/1000 + 60*60)
+            startTime = datetime.datetime.utcfromtimestamp(tss).strftime("%Y-%m-%dT%H:00:00")
+
+            # use features start time only if it is later than in the config
+            if startTime > config['startTime']:
+                config['startTime'] = startTime
         except:
-          tss = 1649000000
+            LOGGER.info("Could not read the last time from features file")
 
-    config['startTime'] = datetime.datetime.utcfromtimestamp(tss).strftime("%Y-%m-%dT%H:00:00")
-    #print(config['startTime'])
-
+    # saving config file
     file_json = open(f'{config_folder}/config_leakage_pressure_updated.json', 'w+')
     file_json.write(json.dumps(config, indent=4, sort_keys=True) )
     file_json.close()
 
+    # initializing batch fusion
     sf2 = batchFusion(config)
 
-
+    # main part
     update_outputs = True
     try:
-      fv, t = sf2.buildFeatureVectors()
+        fv, t = sf2.buildFeatureVectors()
     except:
-      print('Feature vector generation failed')
-      update_outputs = False
+        LOGGER.info('Feature vector generation failed')
+        update_outputs = False
 
     if(update_outputs):
 
-      # firs sensor missing therefore zeros for now
-      zero = np.zeros((fv.shape[0], 1))
+        # first sensor missing therefore zeros for now
+        zero = np.zeros((fv.shape[0], 1))
 
-      fv = np.concatenate((zero, fv), axis=1)
+        fv = np.concatenate((zero, fv), axis=1)
 
-      # use formula to transform TODO add in sistem
-      fv[:, 1:4] = (fv[:,1:4] - 0.6) * 4 * 10.197
+        # use formula to transform TODO add in system
+        fv[:, 1:4] = (fv[:,1:4] - 0.6) * 4 * 10.197
 
 
-      with open(f'{features_folder}/features_pressure_updated.json', 'a+') as file_json:
+        with open(f'{features_folder}/features_pressure_updated.json', 'a+') as file_json:
+            for j in range(t.shape[0]):
+                fv_line = {"timestamp":int(t[j].astype('uint64')/1000000), "ftr_vector":list(fv[j])}
+
+                #data is uploaded at different times - this ensures that FV's won't be sent if data hasn't been uploaded for one or more of the sensors
+                last_values = []
+                for n in range(7):
+                    last_values.append(fv[j][n])
+                last_values = np.array(last_values)
+
+                if((all(isinstance(x, (float, int)) for x in last_values)) and (not np.isnan(last_values).any())):
+                    file_json.write((json.dumps(fv_line) + '\n' ))
+            file_json.close()
+
+
         for j in range(t.shape[0]):
-            fv_line = {"timestamp":int(t[j].astype('uint64')/1000000), "ftr_vector":list(fv[j])}
+            output = {"timestamp":int(t[j].astype('uint64')/1000000), "ftr_vector":list(fv[j])}
+            output_topic = "features_braila_leakage_detection_updated"
 
-            #data is uploaded at different times - this ensures that FV's won't be sent if data hasn't been uploaded for one or more of the sensors
             last_values = []
             for n in range(7):
-              last_values.append(fv[j][n])
+                last_values.append(fv[j][n])
             last_values = np.array(last_values)
 
-
+            # sending to kafka if everything is allright
             if((all(isinstance(x, (float, int)) for x in last_values)) and (not np.isnan(last_values).any())):
-              file_json.write((json.dumps(fv_line) + '\n' ))
+                future = producer.send(output_topic, output)
 
+            try:
+                record_metadata = future.get(timeout=10)
+            except Exception as e:
+                LOGGER.error('Producer error: ' + str(e))
 
-      file_json.close()
-
-
-      for j in range(t.shape[0]):
-          output = {"timestamp":int(t[j].astype('uint64')/1000000), "ftr_vector":list(fv[j])}
-          output_topic = "features_braila_leakage_detection_updated"
-
-          last_values = []
-          for n in range(7):
-            last_values.append(fv[j][n])
-          last_values = np.array(last_values)
-
-         # Start Kafka producer
-          if((all(isinstance(x, (float, int)) for x in last_values)) and (not np.isnan(last_values).any())):
-            future = producer.send(output_topic, output)
-
-          try:
-              record_metadata = future.get(timeout=10)
-          except Exception as e:
-              print('Producer error: ' + str(e))
-
-#Do batch fusion once per day
+# schedule batch once per day
 schedule.every().day.at("09:00").do(RunBatchFusionOnce)
-print(schedule.get_jobs())
-now = datetime.datetime.now()
-
-current_time = now.strftime("%H:%M:%S")
-#print("Current Time =", current_time)
-
 RunBatchFusionOnce()
-while True:
+LOGGER.info("Component started successfully")
 
+while True:
     schedule.run_pending()
     time.sleep(1)
